36ad8214465f5c03ef8b14c9e791404c7385a6c8
iamsaief <saiefalemon@gmail.com>
2023-04-19 06:01:26 +0600

    ui integration and dispatch action - videosSlice

--- minitube-rtk/src/components/grid/VideoGrid.jsx
+++ minitube-rtk/src/components/grid/VideoGrid.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import VideoGridItem from './VideoGridItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchVideos } from '../../features/videos/videosSlice';
+import Loading from '../ui/Loading';
 
 function VideoGrid(props) {
+	const dispatch = useDispatch();
+	const { videos, isLoading, isError, error } = useSelector((state) => state.videos);
+
+	useEffect(() => {
+		dispatch(fetchVideos());
+	}, [dispatch]);
+
+	// logic for what to render
+	let content;
+	if (isLoading) {
+		content = <Loading />;
+	}
+	if (!isLoading && isError) {
+		content = <div className="col-span-12">{error}</div>;
+	}
+	if (!isError && !isLoading && videos?.length === 0) {
+		content = <div className="col-span-12">No videos found!</div>;
+	}
+	if (!isError && !isLoading && videos?.length > 0) {
+		content = videos.map((video) => <VideoGridItem key={video.id} video={video} />);
+	}
+
 	return (
 		<section className="pt-12">
 			<section className="pt-12">
-				<div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-					<VideoGridItem />
-
-					{/* <!-- error section
-                    <div className="col-span-12">some error happened</div> --> */}
-				</div>
+				<div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">{content}</div>
 			</section>
 		</section>
 	);
---
+++ minitube-rtk/src/components/ui/Loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Loading(props) {
+	return <div className="col-span-12">Loading ...</div>;
+}
+
+export default Loading;
