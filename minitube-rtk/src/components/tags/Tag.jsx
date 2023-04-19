826bf4e15ea965384e24b8dc2587b4bc03506424
iamsaief <saiefalemon@gmail.com>
2023-04-20 02:15:09 +0600

    fetch tags from backend and ui integration

--- minitube-rtk/src/components/tags/Tag.jsx
+++ minitube-rtk/src/components/tags/Tag.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Tag(props) {
-	return <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">react</div>;
+function Tag({ title }) {
+	return <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">{title}</div>;
 }
 
 export default Tag;
--- minitube-rtk/src/components/tags/Tags.jsx
+++ minitube-rtk/src/components/tags/Tags.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tag from './Tag';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTags } from '../../features/tags/tagsSlice';
 
 function Tags(props) {
-	return (
+	const { tags } = useSelector((state) => state.tags);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchTags());
+	}, [dispatch]);
+
+	return tags?.length > 0 ? (
 		<section>
 			<div className="max-w-7xl mx-auto px-4 xl:px-0 py-6 flex gap-2 border-b overflow-y-auto">
-				<Tag />
-				<Tag />
+				{tags.map((tag) => (
+					<Tag key={tag.id} title={tag.title} />
+				))}
 			</div>
 		</section>
-	);
+	) : null;
 }
 
 export default Tags;
